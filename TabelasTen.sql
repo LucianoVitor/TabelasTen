b79fda42091884d70aa8602c72958ee16c088f4d
LucianoVitor <99770675+LucianoVitor@users.noreply.github.com>
2022-03-24 19:50:12 -0300

    Add files via upload

---
+++ TabelasTen.sql
@@ -0,0 +1,71 @@
+create database Animes;
+
+create table Luffy
+
+(
+    Nome varchar(255) not null,
+
+);
+
+create table Zoro
+
+(
+    Nome varchar(255) not null,
+
+);
+
+create table Sanji
+
+(
+    Nome varchar(255) not null,
+
+);
+
+create table Naruto
+
+(
+    Nome varchar(255) not null,
+
+);
+
+create table 002
+
+(
+    Nome varchar(255) not null,
+
+);
+
+create table Goku
+
+(
+    Nome varchar(255) not null,
+
+);
+
+create table Law
+
+(
+    Nome varchar(255) not null,
+
+);
+
+create table Ace
+
+(
+    Nome varchar(255) not null,
+
+);
+
+create table Kuririn
+
+(
+    Nome varchar(255) not null,
+
+);
+
+create table Gohan
+
+(
+    Nome varchar(255) not null,
+
+);
